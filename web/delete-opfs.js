e716c83a8acb817859e9373c6d5a21f9da2ba137
idutsu <email@idutsu.com>
2026-05-05 23:37:50 +0900

    update

--- web/delete-opfs.js
+++ web/delete-opfs.js
@@ -1,11 +1,8 @@
 (async () => {
     const root = await navigator.storage.getDirectory();
-
-    // OPFSのルートディレクトリの中身を順番に全て削除する
     for await (const name of root.keys()) {
         await root.removeEntry(name, { recursive: true });
-        console.log(`削除完了: ${name}`);
+        console.log(`OPFSの ${name}　を削除しました`);
     }
-
-    console.log("OPFSのデータをすべて削除しました！ページをリロードしてください。");
+    console.log("OPFSのデータをすべて削除しました");
 })();
